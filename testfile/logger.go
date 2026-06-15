522af9d384ab49f8616763b1050c5056d170b753
Peacemaker-DB <denisBA1995@gmail.com>
2026-06-16 00:58:58 +0300

    Refactor logger initialization

--- testfile/logger.go
+++ testfile/logger.go
@@ -7,27 +7,36 @@ import (
 
 var Log *zap.Logger
 
-// Init инициализирует глобальный логгер; call once (например в main).
 func Init(dev bool) error {
-	var cfg zap.Config
-	if dev {
-		cfg = zap.NewDevelopmentConfig()
-	} else {
-		cfg = zap.NewProductionConfig()
-		cfg.EncoderConfig.TimeKey = "ts"
-		cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-	}
-	l, err := cfg.Build(zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
+	cfg := newLoggerConfig(dev)
+
+	logger, err := cfg.Build(zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
 	if err != nil {
 		return err
 	}
-	zap.ReplaceGlobals(l) // optional
-	Log = l
+
+	zap.ReplaceGlobals(logger)
+	Log = logger
+
 	return nil
 }
 
 func Sync() {
-	if Log != nil {
-		_ = Log.Sync()
+	if Log == nil {
+		return
 	}
+
+	_ = Log.Sync()
+}
+
+func newLoggerConfig(dev bool) zap.Config {
+	if dev {
+		return zap.NewDevelopmentConfig()
+	}
+
+	cfg := zap.NewProductionConfig()
+	cfg.EncoderConfig.TimeKey = "ts"
+	cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	return cfg
 }
